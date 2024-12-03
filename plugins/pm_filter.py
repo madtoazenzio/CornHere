0e3a133793dc2be037ed35f6112613305a080d69
メ™Asʏɴ Eᴅɪᴛᴢ​ゞメ <89691212+madtoazenzio@users.noreply.github.com>
2024-12-03 18:10:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -893,7 +893,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('🔎 𝖲𝖾𝖺𝗋𝖼𝗁 𝖨𝗇 𝖬𝗒 𝖨𝗇𝗅𝗂𝗇𝖾', switch_inline_query_current_chat='')
                 ],[
                     InlineKeyboardButton('🛡 𝖮𝗐𝗇𝖾𝗋', callback_data="owner_info"),
                     InlineKeyboardButton('🧩 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉', url=f"https://t.me/{SUPPORT_CHAT}")
